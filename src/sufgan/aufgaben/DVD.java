1fa9476041ce67185221496cf8b348cce8bfcfb1
sufgan <sila2005sad@gmail.com>
2023-09-25 12:38:00 +0200

    25.09.2023

---
+++ src/sufgan/aufgaben/DVD.java
@@ -0,0 +1,22 @@
+package sufgan.aufgaben;
+
+import java.util.Scanner;
+
+public class DVD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Wie Viel DVD haben Sie gekauft?");
+		int DVDCount = scanner.nextInt();
+		
+		double DVDKosten = 2.30;
+		if (DVDCount > 2) DVDKosten -= 0.50;
+		if (DVDCount > 5) DVDKosten -= 0.20;
+		
+		System.out.printf("Zahlen bitte %.2f", DVDKosten * DVDCount);
+
+	}
+
+}
